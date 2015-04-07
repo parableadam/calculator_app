fd8e065be88f3e2b3230faf4201d18b4ef6b5833
Adam Ryan <adam@parablesoft.com>
2015-04-07 08:18:04 -0400

    Styling changes

--- calculate.rb
+++ calculate.rb
@@ -1,5 +1,5 @@
 def valid_operator?(operator)
-  ["add", "subtract", "multiply", "divide"].include? operator
+  ["add", "subtract", "multiply", "divide"].include? operator.downcase
 end
 
 puts "Choose from the following operators: add, subtract, multiply, or divide"
@@ -11,9 +11,10 @@ while not valid_operator? operator
   operator = gets.chomp
 end
 
-if valid_operator? operator
+#if valid_operator? operator (This is not need since I've invalidated the use of
+#any user input other than what's defined as a valid_operator?  
   puts "What is the first number you'd like to use?"
-end
+#end
 
 num1 = gets.chomp
   
@@ -21,22 +22,34 @@ num1 = gets.chomp
 
 num2 = gets.chomp
 
-if operator.downcase ==  'add'
-  result = num1.to_i + num2.to_i
-elsif operator.downcase == "subtract"
-  result = num1.to_i - num2.to_i
-elsif operator.downcase == "multiply"
-  result = num1 * num2
-elsif operator.downcase == "divide"
-  result = num1.to_f / num2.to_f
-end
+#if operator.downcase ==  'add'
+  #result = num1.to_i + num2.to_i
+#elsif operator.downcase == "subtract"
+  #result = num1.to_i - num2.to_i
+#elsif operator.downcase == "multiply"
+  #result = num1 * num2
+#elsif operator.downcase == "divide"
+  #result = num1.to_f / num2.to_f
+#end
+
+result = 
+  case operator.downcase
+  when 'add'
+    num1.to_i + num2.to_i
+  when 'subtract'
+    num1.to_i - num2.to_i
+  when 'multiply'
+    num1.to_i * num2.to_i
+  when 'divide'
+    num1.to_f / num2.to_f
+  end
 
 
 
 puts "Your result is #{result}"
 
 
-
+  
   
 
 
